2af7cecbad4a67fcef02a2889c7a63394bd73b6b
Izaias Junior <up1.izaiasjr@gmail.com>
2022-07-22 20:14:34 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <h2>Aqui é a Header</h2>
+      </header>
+    );
+  }
+}
